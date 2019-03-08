9889971739a4c2cd97deb7cd4cdfc3d9cb9cf092
raynardlucky <rayluck25@gmail.com>
2019-03-08 13:17:19 +0700

    initial commit

--- app/build/generated/source/r/debug/com/example/android/droidcafe/R.java
+++ app/build/generated/source/r/debug/com/example/android/droidcafe/R.java
@@ -3850,6 +3850,7 @@ public final class R {
     public static final int same_day_messenger_service=0x7f0e0045;
     public static final int search_menu_title=0x7f0e0046;
     public static final int status_bar_notification_info_overflow=0x7f0e0047;
+    public static final int title_activity_order=0x7f0e0048;
   }
   public static final class style {
     public static final int AlertDialog_AppCompat=0x7f0f0000;
